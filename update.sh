9c491e7ec43fef58061091add8b5f4dc3ee28981
erd <erd@conceptual.ca>
2025-05-09 15:15:58 -0400

    updated for modpack 2.0.0-beta

--- update.sh
+++ update.sh
@@ -11,21 +11,20 @@ mkdir -p ./data/mods/
 mkdir -p ./downloads/
 
 
-wget https://raw.githubusercontent.com/myrddraall/mc-moddded-lite-server/main/docker-compose.yml -O docker-compose.yml
+# wget https://raw.githubusercontent.com/myrddraall/mc-moddded-lite-server/main/docker-compose.yml -O docker-compose.yml
 
-wget https://raw.githubusercontent.com/myrddraall/mc-moddded-lite-server/main/start.sh -O start.sh
-chmod +x start.sh
+# wget https://raw.githubusercontent.com/myrddraall/mc-moddded-lite-server/main/start.sh -O start.sh
+# chmod +x start.sh
 
-wget https://raw.githubusercontent.com/myrddraall/mc-moddded-lite-server/main/stop.sh -O stop.sh
-chmod +x stop.sh
+# wget https://raw.githubusercontent.com/myrddraall/mc-moddded-lite-server/main/stop.sh -O stop.sh
+# chmod +x stop.sh
 
-wget https://raw.githubusercontent.com/myrddraall/mc-moddded-lite-server/main/logs.sh -O logs.sh
-chmod +x stop.sh
+# wget https://raw.githubusercontent.com/myrddraall/mc-moddded-lite-server/main/logs.sh -O logs.sh
+# chmod +x stop.sh
 
 # Download mods that can't be auo loaded with curseforge due to licencing 
 
-wget https://raw.githubusercontent.com/myrddraall/mc-moddded-lite-server/main/downloads/HammersAndExcavators-1.1.jar -O downloads/HammersAndExcavators-1.1.jar
-wget https://raw.githubusercontent.com/myrddraall/mc-moddded-lite-server/main/downloads/iron_ender_chests-1.20-1.0.2.jar -O downloads/iron_ender_chests-1.20-1.0.2.jar
-wget https://raw.githubusercontent.com/myrddraall/mc-moddded-lite-server/main/downloads/trimeffects-1.0.1-forge.jar -O downloads/trimeffects-1.0.1-forge.jar
+wget https://cdn.modrinth.com/data/gu7yAYhd/versions/GYnSPc0b/cc-tweaked-1.20.1-forge-1.115.1.jar -O downloads/cc-tweaked-1.20.1-forge-1.115.1.jar
 
-./start.sh
+
+# ./start.sh
