810cbd2e408bf716c029e7071ed4e4e4143741f6
erd <erd@conceptual.ca>
2025-05-10 13:49:13 -0400

    server config

--- update.sh
+++ update.sh
@@ -9,6 +9,7 @@ rm -Rf ./downloads/
 
 mkdir -p ./data/mods/
 mkdir -p ./downloads/
+mkdir -p ./data/world-2025/serverconfig/
 
 
 # wget https://raw.githubusercontent.com/myrddraall/mc-moddded-lite-server/main/docker-compose.yml -O docker-compose.yml
@@ -22,9 +23,12 @@ mkdir -p ./downloads/
 # wget https://raw.githubusercontent.com/myrddraall/mc-moddded-lite-server/main/logs.sh -O logs.sh
 # chmod +x stop.sh
 
+wget https://raw.githubusercontent.com/myrddraall/mc-moddded-lite-server/main/server-config/mantle-server.toml -O ./data/world-2025/serverconfig/mantle-server.toml
+
+
 # Download mods that can't be auo loaded with curseforge due to licencing 
 
 wget https://cdn.modrinth.com/data/gu7yAYhd/versions/GYnSPc0b/cc-tweaked-1.20.1-forge-1.115.1.jar -O downloads/cc-tweaked-1.20.1-forge-1.115.1.jar
 
 
-# ./start.sh
+./start.sh
